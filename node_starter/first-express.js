0352c478e4e8d39ae480be81f9cd4de7658c02f2
NAGERI <33748481+NAGERI@users.noreply.github.com>
2022-02-13 00:30:41 +0300

    added more routes (#1)
    Clear  work

--- node_starter/first-express.js
+++ node_starter/first-express.js
@@ -1,9 +1,14 @@
 const express = require('express')
 
 const app = express()
+
+/*routes */
 app.get('/hello', (req,res) => {
   res.send('<b>Task manager is up and running</b>')
 })
+app.get('/', (req,res) => {
+  res.send('<b>Hello Node world</b>')
+})
 const port = 3000
 
-app.listen(port,console.log(`<b>Server is listening on port ${port}</b>`))
+app.listen(port,console.log(`Server is listening on port ${port}`))
