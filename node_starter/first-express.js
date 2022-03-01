4c051a6d64b3ca922779a76dd4c9ad7b4c841d00
NAGERI <onacedricnageri@gmail.com>
2022-03-01 23:29:42 +0300

    completed taskManager

--- node_starter/first-express.js
+++ node_starter/first-express.js
@@ -12,13 +12,13 @@ app.use(express.static('./public'))
 app.use(express.json())
 app.use(logger('dev'))
 
-/*routes */
+/** routes */
 app.use('/api/v1/tasks',tasks)
 
 app.use(notFound)
 app.use(errorHandlerMiddleware)
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 const start = async () => {
   try {
