c5df9bb8fbc4845a360e9e84ffa572abbaca71b4
NAGERI <onacedricnageri@gmail.com>
2022-03-10 23:42:06 +0300

    sort, select, limit, skip & pagination

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -1,15 +1,22 @@
 const modelProduct = require('../models/product')
 
 const getAllProductsStatic = async (req,res) => {
-  const product = await modelProduct.find({
-    name: 'ikea'
-  })
-  res.status(200).json({msg: 'Products testing route'})
+  const product = await modelProduct.find({ })
+  //.sort('-name price')
+  .select('name price') // Explain this in the API docs
+  .limit() // number of documents to return
+  .skip() // Number of documents returned (sets pagination)
+  res.status(200).json({product})
 }
 
 
 const getAllProducts = async (req,res) => {
-  const {featured,company, name,sort} = req.query
+  const {featured
+        ,company
+        ,name
+        ,sort
+        ,fields
+   } = req.query
   const queryObject = {}
   /*Better approach is setting up an object*/
   if (featured) {
@@ -24,7 +31,28 @@ const getAllProducts = async (req,res) => {
     queryObject.name = { $regex: name, $pattern: 'i'};
   }
   console.log(queryObject)
-  const products = await modelProduct.find(queryObject)
+  let result =  modelProduct.find(queryObject)
+  /**
+   * Adding SORT functionality
+   */
+  if (sort){ // If it has a value!
+    // remove the commma that user puts.
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  }else{
+    result = result.sort('createdAt')
+  }
+  if (fields){ // If it has a value!
+    // remove the commma that user puts.
+    const fieldsList = fields.split(',').join(' ')
+    result = result.select(fieldsList)
+  } 
+  const page = Number(req.query.page)  || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+
+  result = result.skip(skip).limit(limit)
+  const products = await result 
   res.status(200).json({products, nbHits: products.length})
 }
 
